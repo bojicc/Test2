151d9c6b45b6298f587e1a2ac5bb0db5275068a4
Dejan Bojic <dejan.bojic.001@gmail.com>
2023-11-21 18:40:45 +0100

    Added new model OpSystem with properties, ID, Name and Description

---
+++ Test2/Test2/Models/OpSystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test2.Models
+{
+    public class OpSystem
+    {
+        public int OpSystemID { get; set; }
+        public string OpSystemName { get; set; }
+        public string Description { get; set; }
+    }
+}
