041526d8012694638b738db55bf7fd83602dd2ef
Dejan Bojic <dejan.bojic.001@gmail.com>
2023-11-21 18:47:10 +0100

    Added new models. Brand model with properties and model DeviceModel

---
+++ Test2/Test2/Models/Brand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test2.Models
+{
+    public class Brand
+    {
+        public int BrandID { get; set; }
+        public string BrandName { get; set; }
+
+        public ICollection<DeviceModel> DeviceModels { get; set; }
+    }
+}
