d2439cb78bf01e70e258f84e6181bd5d9bc5302c
Dejan Bojic <dejan.bojic.001@gmail.com>
2023-11-22 14:24:46 +0100

    Added DbSet's

---
+++ Test2/Test2/Models/Model1.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Xml;
+
+namespace Test2.Models
+{
+    public class Model1 : DbContext
+    {
+        // Your context has been configured to use a 'Model1' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'Test2.Models.Model1' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'Model1' 
+        // connection string in the application configuration file.
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public virtual DbSet<OpSystem> OpSystems { get; set; }
+        public virtual DbSet<Brand> Brands { get; set; }
+        public virtual DbSet<DeviceModel> DeviceModels { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
